2d1ddde7be7b61201fb97db2e014838cf0568e47
eduardog.cmorales <eduardog.cmorales@gmail.com>
2024-05-18 20:52:41 +0000

    Flask implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import InvitationContainer from './InvitationContainer';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function App() {
+  const { key } = useParams();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`/reactDataDictionaryKey/${key}`)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  }, [key]);
+
   return (
     <div>
-      <h1>Wedding Invitations</h1>
-      <InvitationContainer />
+      {data ? (
+        <div>
+          <h1>{data.name}</h1>
+          <p>{data.invitation}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
