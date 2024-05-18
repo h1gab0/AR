b2eb60763d299a49ed4ff2072f41f6c528567b45
eduardog.cmorales <eduardog.cmorales@gmail.com>
2024-05-18 21:18:04 +0000

    Revert "Flask implementation"
    This reverts commit 2d1ddde7be7b61201fb97db2e014838cf0568e47.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import InvitationContainer from './InvitationContainer';
 
 function App() {
-  const { key } = useParams();
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch(`/reactDataDictionaryKey/${key}`)
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error(error));
-  }, [key]);
-
   return (
     <div>
-      {data ? (
-        <div>
-          <h1>{data.name}</h1>
-          <p>{data.invitation}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <h1>Wedding Invitations</h1>
+      <InvitationContainer />
     </div>
   );
 }
